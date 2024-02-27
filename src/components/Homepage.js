46afc262d6bebb80b64afe53f7dda33958616a4d
Saad-Khan007 <saadkhan322417@gmail.com>
2024-02-27 18:50:36 +0500

    Page 1 UI Changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,20 +5,23 @@ import homesectionimg from '../assets/home-1.webp';
 function Homepage() {
   return (
     <>
-    <div className='content'>
+      <div className='content'>
 
-   
-      <div className='container-section-1'>
-        <img className='img-container' src={homesectionimg} alt="Sample Image"/>
-        <div className='child-container-1'>
-          <div className='left-child-container'>
-            
-          </div>
-          <div className='right-child-container'>
-           
+
+        <div className='container-section-1'>
+          <img className='img-container' src={homesectionimg} alt="Sample Image"/>
+          <div className='child-container-1'>
+            <div className='left-child-container'>
+              Revolutionizing Funding for Social Impact
+            </div>
+            <div className='right-child-container'>
+              We're leveraging tech and Web3 to democratize funding for civil society.
+            </div>
           </div>
         </div>
-      </div>
+        <div className='btn-container'>
+          <button>Get in touch</button>
+        </div>
       </div>
     </>
   );
